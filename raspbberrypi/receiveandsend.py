964dea2c90941ef2a9c29dc08e00cf70e10359e1
amoghstayade <tayade.amogh@gmail.com>
2022-07-21 16:03:25 -0500

    rpi update

--- raspbberrypi/receiveandsend.py
+++ raspbberrypi/receiveandsend.py
@@ -13,12 +13,12 @@ eventhub_name = os.getenv('EVENTHUB_NAME')
 client = EventHubProducerClient.from_connection_string(connection_str, eventhub_name=eventhub_name)
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyACM0', 9600)
     ser.reset_input_buffer()
 
     while True:
         if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
+            line = ser.readline()
             print(line)
             print("SENDING TO EVENTHUB")
             event_data_batch = client.create_batch()
