4bab92e5531a249a7ee2a28f86ee3f502c5062b9
amoghstayade <tayade.amogh@gmail.com>
2022-07-21 16:21:24 -0500

    try except

--- raspbberrypi/receiveandsend.py
+++ raspbberrypi/receiveandsend.py
@@ -14,15 +14,22 @@ client = EventHubProducerClient.from_connection_string(connection_str, eventhub_
 
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600)
-    ser.reset_input_buffer()
+#    ser.reset_input_buffer()
 
     while True:
-        if ser.in_waiting > 0:
-            line = ser.readline()
-            print(line)
-            print("SENDING TO EVENTHUB")
-            event_data_batch = client.create_batch()
-            event_data_batch.add(EventData(line))
-            with client:
-                client.send_batch(event_data_batch)
-                print("sent to eventhub")
+#        if ser.in_waiting > 0:
+       try:
+          line = ser.readline().decode('utf-8')
+          print(line[:3])
+          if (line[0]!="T"):
+             print("waste line")
+          else:
+             print(line)
+             print("SENDING TO EVENTHUB")
+#            event_data_batch = client.create_batch()
+#            event_data_batch.add(EventData(line))
+#            with client:
+#                client.send_batch(event_data_batch)
+#                print("sent to eventhub")
+        except:
+           print("invalid line")
